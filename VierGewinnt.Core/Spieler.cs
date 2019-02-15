0f43ca320687513ed26fe92f6ef8d03e597acd99
Hummel <chrisschulz@gmx.net>
2019-02-15 13:18:10 +0100

    #2

--- VierGewinnt.Core/Spieler.cs
+++ VierGewinnt.Core/Spieler.cs
@@ -7,9 +7,30 @@ using System.Threading.Tasks;
 namespace VierGewinnt.View
 {
    public class Spieler
-    { 
-        private readonly IReadOnlyList<Spielstein> _spielsteine;
-        
+    {
+        private readonly IList<Spielstein> _spielsteine;
+        private readonly string _name;
 
+        public Spieler(IList<Spielstein> spielstein, string name)
+        {
+            if (spielstein == null) throw new ArgumentNullException(nameof(spielstein));
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+
+            _spielsteine = spielstein;
+            _name = name;
+        }
+        public string Name => _name;
+
+        public IList<Spielstein> Spielsteine => _spielsteine;
+      
+        public void SpieleZug(Spalte spalte)
+        {
+            if (spalte == null) throw new ArgumentNullException(nameof(spalte));
+           
+            var obersterSpielstein = _spielsteine[0];
+            _spielsteine.RemoveAt(0);
+            
+            spalte.LasseSpielsteinFallen(obersterSpielstein);
+        }
     }
 }
