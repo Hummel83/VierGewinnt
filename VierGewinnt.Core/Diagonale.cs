63e3d29dcf5253f32d0a5613b2035f3e4eaa90b1
Christian Schulz <chrisschulz@gmx.net>
2019-03-30 09:54:33 +0100

    add missing class

---
+++ VierGewinnt.Core/Diagonale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace VierGewinnt.Core
+{
+    public class Diagonale
+    {
+        private List<Platz> diagnalenPlaetze;
+
+        public Diagonale(List<Platz> diagnalenPlaetze)
+        {
+            this.diagnalenPlaetze = diagnalenPlaetze;
+        }
+    }
+}
